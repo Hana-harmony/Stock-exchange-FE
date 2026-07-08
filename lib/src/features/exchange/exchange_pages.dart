dacff43a77eef860675782f337529a46f4af33c1
1233day <1233day@naver.com>
2026-07-08 12:54:41 +0900

    fix: 세무 OCR 화면 분석 경고 제거

--- lib/src/features/exchange/exchange_pages.dart
+++ lib/src/features/exchange/exchange_pages.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:math' as math;
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
